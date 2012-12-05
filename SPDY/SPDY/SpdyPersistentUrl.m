f6f901f121dd9b86d7acc4ecc4e2565d7d9c7b1b
Brian Martin <brian@locationlabs.com>
2012-12-05 13:12:39 -0800

    flesh out the error handling in SpdyPersistentUrl a lot
     - implement an exponential backoff for retries
     - classifiy errors in a more flexible format, into more categories
     - make some tweaks to the reachability code

--- SPDY/SPDY/SpdyPersistentUrl.m
+++ SPDY/SPDY/SpdyPersistentUrl.m
@@ -6,11 +6,13 @@
 
 @implementation SpdyPersistentUrl {
   NSTimer * pingTimer;
+  NSTimer * retryTimer;
   BOOL stream_closed;
   SCNetworkReachabilityRef reachabilityRef;
   SpdyNetworkStatus networkStatus;
+  int num_reconnects;
+  NSDictionary * errorDict;
 }
-
 
 static void PrintReachabilityFlags(SCNetworkReachabilityFlags    flags)
 {
@@ -46,11 +48,17 @@ static void PrintReachabilityFlags(SCNetworkReachabilityFlags    flags)
     networkStatus = newStatus;
     [self teardown];
   } else if(oldStatus == kSpdyNotReachable) {
-    SPDY_LOG(@"were not reachable, now we are, reconnect");
-    // were not reachable, now we are, reconnect
-    networkStatus = newStatus;
-    [self teardown];
-    [self reconnect:nil];
+    SPDY_LOG(@"were not reachable, now we are");
+    if(self.connectState == kSpdyConnected) {
+      SPDY_LOG(@"BUT we think we were already connected, sending ping");
+      [self sendPing];
+    } else {
+      SPDY_LOG(@"reconnect");
+      // were not reachable, now we are, reconnect
+      networkStatus = newStatus;
+      [self teardown];
+      [self reconnect:nil];
+    }
   } else if(oldStatus == kSpdyReachableViaWiFi && 
 	    newStatus == kSpdyReachableViaWWAN) {
     SPDY_LOG(@"was on wifi, now on wwan, reconnect");
@@ -65,6 +73,8 @@ static void PrintReachabilityFlags(SCNetworkReachabilityFlags    flags)
     // the new status because we are still relying upon the old status (3g)
 
     // XXX make sure that 3g is still valid here (send ping??)
+    SPDY_LOG(@"sending ping to validate 3g connection");
+    [self sendPing];
   } else {
     SPDY_LOG(@"ignoring reachability state change: (%d => %d)", oldStatus, newStatus);
   }
@@ -125,80 +135,169 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
   }
 }
 
--(void)reconnect:(NSError*)error {
-  SPDY_LOG(@"reconnect:%@", error);
+#define RECOVERABLE_FAILURE 1
+#define TRANSIENT_FAILURE   2
+#define HARD_FAILURE        3
+#define INTERNAL_FAILURE    4
+#define UNHANDLED_FAILURE   5
 
-  // XXX make sure this is not a fatal error (e.g. host down, etc).
-  if(error != nil) {
-    if([error.domain isEqualToString:kSpdyErrorDomain]) {
-      if(error.code == kSpdyRequestCancelled) {
-	// in this case, we want to suppress the error
-	// this is because on fatal errors we call
-	// [SpdyStream cancelStream] which sends us this (also fatal) error
-	// if we don't ignore it here, we will loop.
-	return;
-      } else if(error.code == kSpdyConnectionFailed ||
-		error.code == kSpdyConnectionNotSpdy ||
-		error.code == kSpdyInvalidResponseHeaders ||
-		error.code == kSpdyHttpSchemeNotSupported ||
-		error.code == kSpdyStreamClosedWithNoRepsonseHeaders ||
-		error.code == kSpdyVoipRequestedButFailed) {
-	// call fatal error callback
-	if(self.fatalErrorCallback != nil) {
-	  self.fatalErrorCallback(error);
-	}
-	[self teardown];
-	return;
-      }
-    } else if([error.domain isEqualToString:NSPOSIXErrorDomain]) { 
-      if(error.code == ECONNREFUSED || // connection refused
-	 error.code == EHOSTDOWN || // host is down
-	 error.code == EHOSTUNREACH || // no route to host 
-	 error.code == EPFNOSUPPORT ||
-	 error.code == ESOCKTNOSUPPORT ||
-	 error.code == ENOTSUP ||
-	 error.code == ENOTSOCK ||
-	 error.code == EDESTADDRREQ ||
-	 error.code == EMSGSIZE ||
-	 error.code == EPROTOTYPE ||
-	 error.code == ENOPROTOOPT
-	 ) { 
-	if(self.fatalErrorCallback != nil) {
-	  self.fatalErrorCallback(error);
-	}
-	[self teardown];
-	return;
-      }
-    } else if([error.domain isEqualToString:@"kCFStreamErrorDomainNetDB"]) { 
-      if(error.code == HOST_NOT_FOUND || /* Authoritative Answer Host not found */
-	 error.code == NO_RECOVERY || /* Non recoverable errors, FORMERR,REFUSED,NOTIMP*/
-	 error.code == NO_DATA || /* Valid name, no data record of requested type */
-	 error.code == EAI_ADDRFAMILY || /* address family for hostname not supported */
-	 error.code == EAI_BADFLAGS || /* invalid value for ai_flags */
-	 error.code == EAI_FAIL || /* non-recoverable failure in name resolution */
-	 error.code == EAI_FAMILY || /* ai_family not supported */
-	 error.code == EAI_NODATA || /* no address associated with hostname */
-	 error.code == EAI_NONAME || /* hostname nor servname provided, or not known */
-	 error.code == EAI_SERVICE || /* servname not supported for ai_socktype */
-	 error.code == EAI_SOCKTYPE || /* ai_socktype not supported */
-	 error.code == EAI_SYSTEM || /* system error returned in errno */
-	 error.code == EAI_BADHINTS || /* invalid value for hints */
-	 error.code == EAI_PROTOCOL || /* resolved protocol is unknown */
-	 error.code == EAI_OVERFLOW /* argument buffer overflow */
-	 ) { 
-	if(self.fatalErrorCallback != nil) {
-	  self.fatalErrorCallback(error);
-	}
-	[self teardown];
-	return;
-      }
+#define MAX_RECOVERABLE_RECONNECTS 50
+#define MAX_TRANSIENT_RECONNECTS 10
 
-    }
+-(NSDictionary*)errorDict {
+  // here we build an NSDictionary which maps from NSError objects
+  // to one of the failure types above
 
-    // XXX add more cases here
+  if(errorDict == nil) {
+    errorDict = @{
+
+      // these are errors from the spdy layer 
+      kSpdyErrorDomain : @{ 
+	/* the connection failed for some reason */
+	@((int)kSpdyConnectionFailed) : @RECOVERABLE_FAILURE,
+
+	/* the url does not support spdy */
+	@((int)kSpdyConnectionNotSpdy) : @HARD_FAILURE,
+	
+	/* invalid data received for the response headers */
+	@((int)kSpdyInvalidResponseHeaders) : @RECOVERABLE_FAILURE,
+
+	/* url has something other than http or https */
+	@((int)kSpdyHttpSchemeNotSupported) : @HARD_FAILURE,
+
+	/* stream was closed with no data sent */
+	@((int)kSpdyStreamClosedWithNoRepsonseHeaders) : @RECOVERABLE_FAILURE,
+
+	/* we asked for voip, but didn't get it */
+	@((int)kSpdyVoipRequestedButFailed) : @HARD_FAILURE,
+
+	/* we ignore these, they happen every time we call [self teardown] */
+	@((int)kSpdyRequestCancelled) : @INTERNAL_FAILURE,
+      },
+      
+      // these are bsd level errors
+      NSPOSIXErrorDomain : @{ 
+	/* connection refused */
+	@ECONNREFUSED : @TRANSIENT_FAILURE,
+
+	/* host is down */
+	@EHOSTDOWN : @TRANSIENT_FAILURE,
+
+	/* no route to host */
+	@EHOSTUNREACH : @HARD_FAILURE,
+
+	/* Protocol family not supported */
+	@EPFNOSUPPORT : @HARD_FAILURE,
+
+	/* Socket type not supported */
+	@ESOCKTNOSUPPORT : @HARD_FAILURE,
+	
+	/* Operation not supported */
+	@ENOTSUP : @HARD_FAILURE,
+
+	/* Socket operation on non-socket */
+	@ENOTSOCK : @HARD_FAILURE,
+
+	/* Destination address required */
+	@EDESTADDRREQ : @HARD_FAILURE,
+
+	/* Message too long */
+	@EMSGSIZE : @HARD_FAILURE,
+
+	/* Protocol wrong type for socket */
+	@EPROTOTYPE : @HARD_FAILURE,
+
+	/* Protocol not available */
+	@ENOPROTOOPT : @HARD_FAILURE,
+
+	/* Network is down */
+	@ENETDOWN : @TRANSIENT_FAILURE,
+
+	/* Network is unreachable */
+	@ENETUNREACH : @TRANSIENT_FAILURE,
+
+	/* Network dropped connection on reset */
+	@ENETRESET : @RECOVERABLE_FAILURE,
+
+	/* Software caused connection abort */
+	@ECONNABORTED : @RECOVERABLE_FAILURE,
+
+	/* Connection reset by peer */
+	@ECONNRESET : @TRANSIENT_FAILURE,
+
+	/* No buffer space available */
+	@ENOBUFS : @RECOVERABLE_FAILURE,
+
+	/* Socket is already connected */
+	@EISCONN : @INTERNAL_FAILURE,
+
+	/* Socket is not connected */
+	@ENOTCONN : @RECOVERABLE_FAILURE,
+
+	/* Can't send after socket shutdown */
+	@ESHUTDOWN : @RECOVERABLE_FAILURE,
+
+	/* Operation timed out */
+	@ETIMEDOUT : @RECOVERABLE_FAILURE,
+
+	/* Broken pipe */
+	@EPIPE : @RECOVERABLE_FAILURE,
+      },
+      
+      @"kCFStreamErrorDomainNetDB" : @{
+	/* Authoritative Answer Host not found */
+	@HOST_NOT_FOUND : @HARD_FAILURE,
+
+	/* Non recoverable errors, FORMERR,REFUSED,NOTIMP*/
+	@NO_RECOVERY : @HARD_FAILURE,
+
+	/* Valid name, no data record of requested type */
+	@NO_DATA : @HARD_FAILURE,
+
+	/* address family for hostname not supported */
+	@EAI_ADDRFAMILY : @HARD_FAILURE,
+	
+	/* invalid value for ai_flags */
+	@EAI_BADFLAGS : @HARD_FAILURE,
+
+	/* non-recoverable failure in name resolution */
+	@EAI_FAIL : @HARD_FAILURE,
+	
+	/* ai_family not supported */
+	@EAI_FAMILY : @HARD_FAILURE,
+	
+	/* no address associated with hostname */
+	@EAI_NODATA : @HARD_FAILURE,
+	
+	/* hostname nor servname provided, or not known */
+	@EAI_NONAME : @HARD_FAILURE,
+
+	/* servname not supported for ai_socktype */
+	@EAI_SERVICE : @HARD_FAILURE,
+
+	/* ai_socktype not supported */
+	@EAI_SOCKTYPE : @HARD_FAILURE,
+
+	/* system error returned in errno */
+	@EAI_SYSTEM : @HARD_FAILURE,
+
+	/* invalid value for hints */
+	@EAI_BADHINTS : @HARD_FAILURE,
+
+	/* resolved protocol is unknown */
+	@EAI_PROTOCOL : @HARD_FAILURE,
+
+	/* argument buffer overflow */
+	@EAI_OVERFLOW : @HARD_FAILURE,
+      },
+
+      // XXX add more cases here
+    };
   }
-  SPDY_LOG(@"error is not fatal");
+  return errorDict;
+}
 
+-(void)reconnect {
   // here we do reconnect logic
   SpdyConnectState connectState = self.connectState;
   if(self.networkStatus == kSpdyNotReachable) {
@@ -207,7 +306,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
   }
 
   if(!stream_closed && connectState == kSpdyConnected) {
-    SPDY_LOG(@"already connected");
+    SPDY_LOG(@"already connected, not reconnecting");
     return;			// already connected;
   }
 
@@ -221,9 +320,115 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
   [self send];
 }
 
+-(int)errorType:(NSError*)error {
+  NSNumber * type = [self errorDict][error.domain][@(error.code)];
+  if(type == nil) return UNHANDLED_FAILURE;
+  return [type intValue];
+}
+
+-(void)dieOnError:(NSError*)error {
+  if(self.fatalErrorCallback != nil) {
+    self.fatalErrorCallback(error);
+  }
+  [self teardown];
+}
+
+-(void)reconnectWithMax:(int)max {
+  if(num_reconnects < max) {
+    num_reconnects++;
+    SPDY_LOG(@"reconnecting on failure for the %dth time", num_reconnects);
+    [self reconnect];
+  } else {
+    SPDY_LOG(@"NOT reconnecting on failure for the %dth time", num_reconnects);
+  }
+}
+
+-(void)transientReconnect {
+  SPDY_LOG(@"transientReconnect");
+  [self reconnectWithMax:MAX_TRANSIENT_RECONNECTS];
+}
+
+-(void)recoverableReconnect {
+  SPDY_LOG(@"recoverableReconnect");
+  [self reconnectWithMax:MAX_RECOVERABLE_RECONNECTS];
+}
+
+-(void)scheduleReconnectWithSelector:(SEL)selector 
+		     initialInterval:(NSTimeInterval)retry_interval
+			   andFactor:(double)factor {
+  // schedule reconnect in the future
+
+  // exponential backoff
+  for(int i = 0 ; i <= num_reconnects ; i++) retry_interval *= factor;
+
+  SPDY_LOG(@"will retry in %lf seconds", retry_interval);
+
+  [retryTimer invalidate];
+  retryTimer = [NSTimer scheduledTimerWithTimeInterval:retry_interval
+			target:self selector:selector
+			userInfo:nil repeats:NO];
+}
+
+-(void)reconnect:(NSError*)error {
+  SPDY_LOG(@"reconnect:%@", error);
+
+  if([self tearingDown]) {
+    SPDY_LOG(@"IGNORING ERROR WHILE TEARING DOWN");
+    return;
+  }
+
+  if(error != nil) {
+    int error_type = [self errorType:error];
+    switch(error_type) {
+
+    case HARD_FAILURE:
+      SPDY_LOG(@"error type is HARD_FAILURE");
+      // No retry is attempted
+      // call fatalErrorCallback block
+      // teardown the session (socket)
+      [self dieOnError:error];
+      break;
+
+      
+    case UNHANDLED_FAILURE:
+      // this is a red flag, these errors should be flagged explicitly
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      SPDY_LOG(@"UNHANDLED_FAILURE UNHANDLED_FAILURE UNHANDLED_FAILURE");
+      [self dieOnError:error];
+      break;
+
+    case INTERNAL_FAILURE:
+      SPDY_LOG(@"error type is INTERNAL_FAILURE");
+      // in this case, we want to suppress the error
+      // this is because on fatal errors we call
+      // [SpdyStream cancelStream] which sends us kSpdyRequestCancelled
+      // which if also marked fatal will cause a loop
+      break;
+
+    case RECOVERABLE_FAILURE:
+      SPDY_LOG(@"error type is RECOVERABLE_FAILURE");
+      [self teardown];
+      [self scheduleReconnectWithSelector:@selector(recoverableReconnect) 
+	    initialInterval:0.2 andFactor:1.6];
+      break;
+
+    case TRANSIENT_FAILURE:
+      SPDY_LOG(@"error type is TRANSIENT_FAILURE");
+      [self teardown];
+      [self scheduleReconnectWithSelector:@selector(transientReconnect) 
+	    initialInterval:0.8 andFactor:1.7];
+      break;
+    }
+  }
+}
+
 -(void)keepalive {
   if(!stream_closed && self.connectState == kSpdyConnected) {
-    pingTimer = [NSTimer timerWithTimeInterval:6 // XXX fudge this interval?
+    pingTimer = [NSTimer scheduledTimerWithTimeInterval:6 // XXX fudge this interval?
 			 target:self selector:@selector(noPingReceived) 
 			 userInfo:nil repeats:NO];
     [self sendPing];
@@ -240,6 +445,11 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
   if (SCNetworkReachabilityGetFlags(reachabilityRef, &flags)) {
     networkStatus = [SPDY networkStatusForReachabilityFlags:flags];
   }
+
+  SPDY_LOG(@"stream was connected");
+
+  [retryTimer invalidate];
+  num_reconnects = 0;
 }
 
 -(void)streamWasClosed {
@@ -267,6 +477,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
 }
 
 -(void)setup {
+  num_reconnects = 0;
   self.voip = YES;
   stream_closed = NO;
   SpdyPersistentUrl * __unsafe_unretained unsafe_self = self;
