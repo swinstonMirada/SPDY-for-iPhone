f49d519a43a238ce29e15facf0ac8d9571118118
Brian Martin <brian@locationlabs.com>
2014-01-09 16:25:18 -0800

    account for the possibility of multiple responses in SpdyBufferedCallback

--- SPDY/SPDY/SpdyBufferedCallback.m
+++ SPDY/SPDY/SpdyBufferedCallback.m
@@ -9,7 +9,6 @@
 @end
 
 @implementation SpdyBufferedCallback {
-  BOOL did_response_callback;
   NSMutableSet * push_callbacks;
 }
 
@@ -22,8 +21,7 @@
   if(self) {
     self.url = nil;
     _headers = NULL;
-    self.body = CFDataCreateMutable(NULL, 0);
-    did_response_callback = NO;
+    self.body = NULL;
   }
   return self;
 }
@@ -57,52 +55,54 @@
 }
 
 - (size_t)onResponseData:(const uint8_t *)bytes length:(size_t)length {
+  if(self.body == NULL) 
+    self.body = CFDataCreateMutable(NULL, 0);
+
   CFDataAppendBytes(self.body, bytes, length);
   //SPDY_LOG(@"appended %zd bytes", length);
 
   //SPDY_LOG(@"headers are %p", self.headers);
-
-  if(!did_response_callback && self.headers != NULL) {
+  
+  if(self.headers == NULL) {
+    SPDY_LOG(@"headers are null");
+  } else {
     NSString* length_str = (NSString*)CFBridgingRelease(CFHTTPMessageCopyHeaderFieldValue(self.headers, CFStringCreateWithCString(NULL,"content-length",kCFStringEncodingUTF8)));
     if(length_str != nil) {
       int content_length = 0;
       sscanf([length_str UTF8String], "%d", &content_length);
-      //SPDY_LOG(@"got content length %d", content_length);
+      SPDY_LOG(@"got content length %d", content_length);
 
       CFIndex current_data_size = CFDataGetLength(self.body);
       if(current_data_size == content_length) {
 	//SPDY_LOG(@"got all the data, doing response callback before stream close");
 	CFHTTPMessageSetBody(self.headers, self.body);
 	[self onResponse:self.headers];
-	did_response_callback = YES;
+        self.body = NULL;
       }
     } else {
-      //NSDictionary * headers = (NSDictionary*)CFBridgingRelease(CFHTTPMessageCopyAllHeaderFields(self.headers));
       //SPDY_LOG(@"did not get content-length");
-      //SPDY_LOG(@"headers: %@", headers);
-
+      //SPDY_LOG(@"headers: %@", (NSDictionary*)CFBridgingRelease(CFHTTPMessageCopyAllHeaderFields(self.headers)));
     }
   }
   return length;
 }
 
 - (void)onStreamClose {
-  if(!did_response_callback) {
-    if(self.headers != NULL && self.body != NULL) {
-      CFHTTPMessageSetBody(self.headers, self.body);
-      [self onResponse:self.headers];
-      did_response_callback = YES;
-    } else {
-      SPDY_LOG(@"stream closing in error state: self.headers are %p, self.body %p", self.headers, self.body);
-      NSDictionary * dict = [[NSDictionary alloc] 
-			      initWithObjectsAndKeys:
-				@"stream closing in error state", @"reason",
-			      (NSData*)self.body, @"data", nil];
-      NSError * error = [[NSError alloc ] initWithDomain:kSpdyErrorDomain
-					  code:kSpdyStreamClosedWithNoRepsonseHeaders
-					  userInfo:dict];
-      [self onError:error];
-    }
+  SPDY_LOG(@"onStreamClose");
+  if(self.headers != NULL && self.body != NULL) {
+    SPDY_LOG(@"doing response callback");
+    CFHTTPMessageSetBody(self.headers, self.body);
+    [self onResponse:self.headers];
+  } else {
+    SPDY_LOG(@"stream closing in error state: self.headers are %p, self.body %p", self.headers, self.body);
+    NSDictionary * dict = [[NSDictionary alloc] 
+                            initWithObjectsAndKeys:
+                              @"stream closing in error state", @"reason",
+                            (NSData*)self.body, @"data", nil];
+    NSError * error = [[NSError alloc ] initWithDomain:kSpdyErrorDomain
+                                        code:kSpdyStreamClosedWithNoRepsonseHeaders
+                                        userInfo:dict];
+    [self onError:error];
   }
 }
 
